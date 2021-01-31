f452b47b184ca861946ef1658ec4daa971b26b1e
Daniela <dhiguitaa17@gmail.com>
2021-01-31 13:09:39 -0500

    provision the vm and build the backend

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
-echo "Hello world!"
+
+#---git
+# yum install -y git
+
+#---go
+yum install -y golang
+
+#---node and npm
+# curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+# yum install -y nodejs
+
+#---vue cli
+# npm install -g @vue/cli
+
+#cloning the app repo
+git clone https://github.com/jdmendozaa/vuego-demoapp.git
+
+#build the backend
+cd vuego-demoapp/server/
+go build
+mv /home/vagrant/vuego-demoapp/server/vuego-demoapp /shared
